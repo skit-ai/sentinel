05e9f9fd1d9f19152a17b7473e523fd747884a2f
Vipul Sharma <vipul.sharma20@gmail.com>
2021-09-20 21:31:48 +0530

    fix: annotation interface for filters

--- sentinel/analyses/base.py
+++ sentinel/analyses/base.py
@@ -1,21 +1,35 @@
 """
 Base class for all monitoring and analysis plugins
 """
-
+from typing import Optional, Callable
 from abc import ABC, abstractmethod
 
-from sentinel.types import DataFrame
+import pandas as pd
+
+from sentinel.types import T
 
 
 class Analysis(ABC):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, successor: Optional[T] = None):
+        self.successor = successor
+
+    def handle(self, df: pd.DataFrame):
+        df = self.process(df)
+        if self.successor:
+            self.successor.handle(df)
+        return df
+
+    @abstractmethod
+    def preprocess(self, df: pd.DataFrame):
         pass
 
     @abstractmethod
-    def preprocess(self, df: DataFrame):
+    def process(self, df: pd.DataFrame):
         pass
 
-    @abstractmethod
-    def process(self, df: DataFrame):
-        pass
+    def annotate(self, df: pd.DataFrame,
+                 df_series: pd.Series,
+                 func: Callable, annotation: str) -> pd.DataFrame:
+        df[annotation] = df_series.apply(func)
 
+        return df
