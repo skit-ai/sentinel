6a958b9389e422b0ff3eb2e6af55f13e0a13ed8a
Vipul Sharma <vipul.sharma20@gmail.com>
2021-09-22 18:48:25 +0530

    add: use kwargs to get customizable confidence threshold

--- sentinel/analyses/confidence.py
+++ sentinel/analyses/confidence.py
@@ -8,6 +8,8 @@ from sentinel.analyses.base import AnalysisBase, AnalysisFactory
 @AnalysisFactory.register(name="low_confidence", description="Low confidence turns")
 class ConfidenceFilter(AnalysisBase):
     def __init__(self, *args, **kwargs):
+        self.confidence_threshold = kwargs.get("confidence_threshold", 95)
+
         super().__init__(*args, **kwargs)
 
     def preprocess(self, df: pd.DataFrame):
@@ -21,9 +23,10 @@ class ConfidenceFilter(AnalysisBase):
 
     def _get_low_confidence(self, item: List):
         if not item:
-            return
+            return None
 
         item = item[0]
         item = sorted(item, key=lambda x: x["confidence"])
-        if item[0]["confidence"] < 95:
+        if item[0]["confidence"] < self.confidence_threshold:
             return {"score": item[0]["confidence"]}
+        return None
